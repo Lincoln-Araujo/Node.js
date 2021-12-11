770e49d337b062ebc49eb731d35308e961d140ec
Lincoln Araujo <72479081+Lincoln-Araujo@users.noreply.github.com>
2021-12-11 17:40:15 -0300

    Create message-mixer.js

---
+++ Encrypting Messages/message-mixer.js
@@ -0,0 +1,50 @@
+// Importing the functions from encryptors.js here.
+let encryptors = require('./encryptors.js');
+
+let caesarCipher = encryptors.caesarCipher;
+
+let symbolCipher = encryptors.symbolCipher;
+
+let reverseCipher = encryptors.reverseCipher;
+
+// User Input / Output Logic
+/////////////////////////////////////////////
+
+const encryptionMethod = getEncryptionMethod();
+process.stdin.on('data', (userInput) => {
+  displayEncryptedMessage(encryptionMethod, userInput);
+});
+
+/* Helper function for determining which cipher method the user chooses when they run the program. */
+function getEncryptionMethod() {
+  let encryptionMethod;
+  
+  const encryptionType = process.argv[2];  
+  if (encryptionType === 'symbol') {
+    encryptionMethod = symbolCipher;
+  } else if (encryptionType === 'reverse') {
+    encryptionMethod = reverseCipher;
+  } else if (encryptionType === 'caesar') {
+    let amount = Number(process.argv[3]);
+    if (Number.isNaN(amount)) {
+      process.stdout.write(`Try again with a valid amount argument. \n`)
+      process.exit();  
+    }
+    encryptionMethod = (str) => caesarCipher(str, amount);
+  } 
+  else {
+    process.stdout.write(`Try again with a valid encryption type. \n`)
+    process.exit();
+  }
+
+  process.stdout.write('Enter the message you would like to encrypt...\n> ');
+  return encryptionMethod;
+}
+
+/* Helper function for displaying the encrypted message to the user. */
+function displayEncryptedMessage(encryptionMethod, userInput) {
+  let str = userInput.toString().trim();    
+  let output = encryptionMethod(str);
+  process.stdout.write(`\nHere is your encrypted message:\n> ${output}\n`)
+  process.exit();
+}
